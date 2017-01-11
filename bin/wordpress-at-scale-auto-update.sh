60d65d54394d58f123485dfc5bb81008d3fa95e2
Josh Shashaty <josh@thewpvalet.com>
2017-01-11 15:33:37 -0500

    uncomment terminus part of the build, remove merge to test/live

--- bin/wordpress-at-scale-auto-update.sh
+++ bin/wordpress-at-scale-auto-update.sh
@@ -8,75 +8,75 @@ UPDATES_APPLIED=true
 echo -e "\nLogging into Terminus..."
 terminus auth login --machine-token=${TERMINUS_MACHINE_TOKEN}
 
-# # delete the multidev environment
-# echo -e "\nDeleting the ${MULTIDEV} multidev environment..."
-# terminus site delete-env --remove-branch --yes
+# delete the multidev environment
+echo -e "\nDeleting the ${MULTIDEV} multidev environment..."
+terminus site delete-env --remove-branch --yes
 
-# # recreate the multidev environment
-# echo -e "\nRe-creating the ${MULTIDEV} multidev environment..."
-# terminus site create-env --from-env=live --to-env=${MULTIDEV}
+# recreate the multidev environment
+echo -e "\nRe-creating the ${MULTIDEV} multidev environment..."
+terminus site create-env --from-env=live --to-env=${MULTIDEV}
 
-# # check for upstream updates
-# echo -e "\nChecking for upstream updates on the ${MULTIDEV} multidev..."
-# # the output goes to stderr, not stdout
-# UPSTREAM_UPDATES=$(terminus site upstream-updates list  --format=bash  2>&1)
+# check for upstream updates
+echo -e "\nChecking for upstream updates on the ${MULTIDEV} multidev..."
+# the output goes to stderr, not stdout
+UPSTREAM_UPDATES=$(terminus site upstream-updates list  --format=bash  2>&1)
 
-# if [[ ${UPSTREAM_UPDATES} == *"No updates"* ]]
-# then
-#     # no upstream updates available
-#     echo -e "\nNo upstream updates found on the ${MULTIDEV} multidev..."
-# else
-# 	# making sure the multidev is in git mode
-# 	echo -e "\nSetting the ${MULTIDEV} multidev to git mode"
-# 	terminus site set-connection-mode --mode=git
+if [[ ${UPSTREAM_UPDATES} == *"No updates"* ]]
+then
+    # no upstream updates available
+    echo -e "\nNo upstream updates found on the ${MULTIDEV} multidev..."
+else
+	# making sure the multidev is in git mode
+	echo -e "\nSetting the ${MULTIDEV} multidev to git mode"
+	terminus site set-connection-mode --mode=git
 	
-#     # apply WordPress upstream updates
-#     echo -e "\nApplying upstream updates on the ${MULTIDEV} multidev..."
-#     terminus site upstream-updates apply --yes --updatedb --accept-upstream --env=${MULTIDEV}
-#     UPDATES_APPLIED=true
-# fi
+    # apply WordPress upstream updates
+    echo -e "\nApplying upstream updates on the ${MULTIDEV} multidev..."
+    terminus site upstream-updates apply --yes --updatedb --accept-upstream --env=${MULTIDEV}
+    UPDATES_APPLIED=true
+fi
 
-# # making sure the multidev is in SFTP mode
-# echo -e "\nSetting the ${MULTIDEV} multidev to SFTP mode"
-# terminus site set-connection-mode --mode=sftp
+# making sure the multidev is in SFTP mode
+echo -e "\nSetting the ${MULTIDEV} multidev to SFTP mode"
+terminus site set-connection-mode --mode=sftp
 
-# # check for WordPress plugin updates
-# echo -e "\nChecking for WordPress plugin updates on the ${MULTIDEV} multidev..."
-# PLUGIN_UPDATES=$(terminus wp "plugin list --field=update" --format=bash)
+# check for WordPress plugin updates
+echo -e "\nChecking for WordPress plugin updates on the ${MULTIDEV} multidev..."
+PLUGIN_UPDATES=$(terminus wp "plugin list --field=update" --format=bash)
 
-# if [[ ${PLUGIN_UPDATES} == *"available"* ]]
-# then
-#     # update WordPress plugins
-#     echo -e "\nUpdating WordPress plugins on the ${MULTIDEV} multidev..."
-#     terminus wp "plugin update --all"
+if [[ ${PLUGIN_UPDATES} == *"available"* ]]
+then
+    # update WordPress plugins
+    echo -e "\nUpdating WordPress plugins on the ${MULTIDEV} multidev..."
+    terminus wp "plugin update --all"
 
-#     # committing updated WordPress plugins
-#     echo -e "\nCommitting WordPress plugin updates on the ${MULTIDEV} multidev..."
-#     terminus site code commit --message="update WordPress plugins" --yes
-#     UPDATES_APPLIED=true
-# else
-#     # no WordPress plugin updates found
-#     echo -e "\nNo WordPress plugin updates found on the ${MULTIDEV} multidev..."
-# fi
+    # committing updated WordPress plugins
+    echo -e "\nCommitting WordPress plugin updates on the ${MULTIDEV} multidev..."
+    terminus site code commit --message="update WordPress plugins" --yes
+    UPDATES_APPLIED=true
+else
+    # no WordPress plugin updates found
+    echo -e "\nNo WordPress plugin updates found on the ${MULTIDEV} multidev..."
+fi
 
-# # check for WordPress theme updates
-# echo -e "\nChecking for WordPress theme updates on the ${MULTIDEV} multidev..."
-# THEME_UPDATES=$(terminus wp "theme list --field=update" --format=bash)
+# check for WordPress theme updates
+echo -e "\nChecking for WordPress theme updates on the ${MULTIDEV} multidev..."
+THEME_UPDATES=$(terminus wp "theme list --field=update" --format=bash)
 
-# if [[ ${THEME_UPDATES} == *"available"* ]]
-# then
-#     # update WordPress themes
-#     echo -e "\nUpdating WordPress plugins on the ${MULTIDEV} multidev..."
-#     terminus wp "theme update --all"
+if [[ ${THEME_UPDATES} == *"available"* ]]
+then
+    # update WordPress themes
+    echo -e "\nUpdating WordPress plugins on the ${MULTIDEV} multidev..."
+    terminus wp "theme update --all"
 
-#     # committing updated WordPress themes
-#     echo -e "\nCommitting WordPress theme updates on the ${MULTIDEV} multidev..."
-#     terminus site code commit --message="update WordPress themes" --yes
-#     UPDATES_APPLIED=true
-# else
-#     # no WordPress theme updates found
-#     echo -e "\nNo WordPress theme updates found on the ${MULTIDEV} multidev..."
-# fi
+    # committing updated WordPress themes
+    echo -e "\nCommitting WordPress theme updates on the ${MULTIDEV} multidev..."
+    terminus site code commit --message="update WordPress themes" --yes
+    UPDATES_APPLIED=true
+else
+    # no WordPress theme updates found
+    echo -e "\nNo WordPress theme updates found on the ${MULTIDEV} multidev..."
+fi
 
 if [[ "${UPDATES_APPLIED}" = false ]]
 then
@@ -126,27 +126,10 @@ else
 
         # enable git mode on dev
         echo -e "\nEnabling git mode on the dev environment..."
-        # terminus site set-connection-mode --env=dev --mode=git --yes
+        terminus site set-connection-mode --env=dev --mode=git --yes
 
         # merge the multidev back to dev
         echo -e "\nMerging the ${MULTIDEV} multidev back into the dev environment (master)..."
-        # terminus site merge-to-dev
-
-        # deploy to test
-        echo -e "\nDeploying the updates from dev to test..."
-        # terminus site deploy --env=test --sync-content --cc --note="Auto deploy of WordPress updates (core, plugin, themes)"
-
-        # backup the live site
-        echo -e "\nBacking up the live environment..."
-        # terminus site backups create --env=live --element=all
-
-        # deploy to live
-        echo -e "\nDeploying the updates from test to live..."
-        # terminus site deploy --env=live --cc --note="Auto deploy of WordPress updates (core, plugin, themes)"
-
-        echo -e "\nVisual regression tests passed! WordPress updates deployed to live..."
-        SLACK_MESSAGE="valet.io Circle CI update check #${CIRCLE_BUILD_NUM} by ${CIRCLE_PROJECT_USERNAME} Visual regression tests passed! WordPress updates deployed to <https://dashboard.pantheon.io/sites/${SITE_UUID}#live/deploys|the live environment>."
-        echo -e "\nSending a message to the ${SLACK_CHANNEL} Slack channel"
-        # curl -X POST --data "payload={\"channel\": \"${SLACK_CHANNEL}\", \"username\": \"${SLACK_USERNAME}\", \"text\": \"${SLACK_MESSAGE}\"}" $SLACK_HOOK_URL
+        terminus site merge-to-dev
     fi
 fi
